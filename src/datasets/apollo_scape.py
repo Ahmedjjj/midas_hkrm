7e8ff43068c578967eeda8a8950ddf0360e4c6df
Ahmed Jellouli <ahmed.jellouli@epfl.ch>
2021-10-14 18:42:48 +0200

    dataset abstraction + utils

---
+++ src/datasets/apollo_scape.py
@@ -0,0 +1,32 @@
+import posixpath
+
+from src.datasets.remote_dataset import RemoteDataset
+
+
+class ApolloScape(RemoteDataset):
+    def __init__(self, remote=False, test=False, username=None):
+        super().__init__(remote, username=username)
+        self._test = test
+
+    @property
+    def path_prefix(self):
+        return '/runai-ivrl-scratch/students/2021-fall-sp-jellouli/mix6/ApolloScape'
+
+    @property
+    def locations(self):
+        if self.test:
+            return [{'imgs': 'test/camera_5', 'labels': None},
+                    {'imgs': 'test/camera_6', 'labels': None}]
+
+        else:
+            return [{'imgs': 'train/stereo_train_001/camera_5', 'labels': 'train/stereo_train_001/disparity'},
+                    {'imgs': 'train/stereo_train_002/camera_5', 'labels': 'train/stereo_train_002/disparity'},
+                    {'imgs': 'train/stereo_train_003/camera_5', 'labels': 'train/stereo_train_003/disparity'},
+                    ]
+
+    def labels_filename(self, img_name):
+        return posixpath.splitext(img_name)[0] + '.png'
+
+    @property
+    def test(self):
+        return self._test
---
+++ src/datasets/remote_dataset.py
@@ -0,0 +1,75 @@
+import os
+import posixpath
+import random
+from abc import ABC, abstractmethod
+
+from torch.utils.data import IterableDataset
+
+from src.utils.img_utils import read_image
+from src.utils.sftp_utils import open_cluster_connection, read_remote_image
+
+
+class RemoteDataset(IterableDataset, ABC):
+
+    def __init__(self, remote, username=None):
+        self._remote = remote
+        if remote:
+            if not username:
+                raise ValueError('username must be provided for remote datasets')
+            self._sftp = open_cluster_connection(username)
+
+    @property
+    def sftp(self):
+        return self._sftp
+
+    @property
+    def is_remote(self):
+        return self._remote
+
+    @property
+    @abstractmethod
+    def path_prefix(self):
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def locations(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def labels_filename(self, image_filename):
+        raise NotImplementedError()
+
+    def __get_all_samples(self):
+        for location_dict in self.locations:
+            img_folder, labels_folder = location_dict.values()
+            img_folder_path = posixpath.join(self.path_prefix, img_folder)
+            images = self.sftp.listdir(img_folder_path) if self.is_remote else os.listdir(img_folder_path)
+            all_samples = [posixpath.join(img_folder_path, image) for image in images]
+            if labels_folder:
+                labels = [self.labels_filename(image) for image in images]
+                all_samples = [(image, os.path.join(self.path_prefix, labels_folder, labels)) for (image, labels) in
+                               zip(all_samples, labels)]
+
+            return all_samples
+
+    def __iter__(self):
+        all_samples = self.__get_all_samples()
+        random.shuffle(all_samples)
+        for sample in all_samples:
+            if type(sample) is tuple:
+                image_path, labels_path = sample
+                if self._remote:
+                    img, labels = read_remote_image(self.sftp, image_path), \
+                                  read_remote_image(self.sftp, labels_path, grayscale=True)
+                else:
+                    img, labels = read_image(image_path), read_image(labels_path, grayscale=True)
+
+                yield img, labels
+            else:
+                image_path = sample
+                if self._remote:
+                    img = read_remote_image(self.sftp, image_path)
+                else:
+                    img = read_image(image_path)
+                yield img
