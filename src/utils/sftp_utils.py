07ca5c004f79abf1c101176b2ce3ebca5f2c323d
Ahmed Jellouli <ahmed.jellouli@epfl.ch>
2021-10-14 19:31:31 +0200

    add possibility to read grayscale images

--- src/utils/sftp_utils.py
+++ src/utils/sftp_utils.py
@@ -8,6 +8,7 @@ import paramiko
 def open_cluster_connection(username):
     password = getpass.getpass(prompt='Remote server password: ')
     client = paramiko.SSHClient()
+    
     client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy)
     client.connect(hostname='iccluster138.iccluster.epfl.ch',
                    username=username,
