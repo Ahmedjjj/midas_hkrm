2b3ce2114eb133c3a0b3dc17cb6af42a1eff9ee9
Jellouli Ahmed <jellouli@iccluster138.iccluster.epfl.ch>
2021-12-10 14:27:18 +0100

    intermediate model, in testing

--- src/depth/midas_hkrm.py
+++ src/depth/midas_hkrm.py
@@ -1,13 +1,17 @@
 import torch.nn as nn
-from nn import functional as F
+import torch.nn.functional as F
 import torch
 import numpy as np
 
-from midas.blocks import _make_encoder
-from src.objects.features import ObjectFeatureExtractor
+from midas.blocks import _make_encoder, FeatureFusionBlock, Interpolate
+from src.objects.features import ObjectDetector
 import torchvision.transforms.functional as TF
 
+from src.utils.errors import require
 import enum
+from typing import List
+
+from src.utils.depth_utils import pad_features, pad_masks
 
 
 class ResNetSizes(enum.Enum):
@@ -25,13 +29,21 @@ class MidasHKRMNet(nn.Module):
     """
     A modified MiDaS network that uses object features as extra feature maps in the decoder
     """
-    def __init__(object_feature_extractor: ObjectFeatureExtractor,
-                 object_feature_dimension: int = 1024,
+
+    def __init__(self,
+                 object_feature_extractor: ObjectDetector,
                  max_objects: int,
+                 object_feature_dimension: int = 1024,
                  pretrained_resnet: bool = True,
-                 features=256):
+                 features=256,
+                 device='cuda'):
 
         super().__init__()
+
+        require(int(np.sqrt(object_feature_dimension)) ** 2 == object_feature_dimension,
+                "object_feature_dimension should be a perfect square")
+
+        self.device = device
 
         # basic resnet encoder + midas specific num channels reduction
         self.backbone, self.channel_reduc = _make_encoder(
@@ -40,16 +52,6 @@ class MidasHKRMNet(nn.Module):
         # backbone object feature extractor
         self.object_feature_extractor = object_feature_extractor
         self.object_feature_dimension = object_feature_dimension
-
-        # project object features into vectors of the desired sizes
-        self.object_feature_mapper_4 = nn.Linear(
-            object_feature_dimension, ResNetSizes.LAYER_4.height * ResNetSizes.LAYER_4.width)
-        self.object_feature_mapper_3 = nn.Linear(
-            object_feature_dimension,  ResNetSizes.LAYER_3.height * ResNetSizes.LAYER_3.width)
-        self.object_feature_mapper_2 = nn.Linear(
-            object_feature_dimension,  ResNetSizes.LAYER_2.height * ResNetSizes.LAYER_2.width)
-        self.object_feature_mapper_1 = nn.Linear(
-            object_feature_dimension,  ResNetSizes.LAYER_1.height * ResNetSizes.LAYER_1.width)
 
         self.max_objects = max_objects
 
@@ -61,25 +63,30 @@ class MidasHKRMNet(nn.Module):
 
         # same as MidaS
         self.output_conv = nn.Sequential(
-            nn.Conv2d(features, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(features + max_objects * 2, 128, kernel_size=3, stride=1, padding=1),
             Interpolate(scale_factor=2, mode="bilinear"),
             nn.Conv2d(128, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(True),
             nn.Conv2d(32, 1, kernel_size=1, stride=1, padding=0),
             nn.ReLU(True)
         )
+        self.to(device)
 
-    def forward(self, x: torch.tensor):
+    def forward(self, object_detector_input: List[np.array], depth_input: torch.tensor):
         """
         Args:
-            x (torch.tensor): tensor containing the input images to the model, assumed to have shape num images x num channels x height x width
-
+            x (List[np.array]): list of images, assumed to have shape height x width x num_channels
+            depth_input (input_of the depth encoder): torch.tensor
         Returns:
             torch.tensor: disparity map
         """
 
+        require(len(object_detector_input) == depth_input.shape[0],
+                "detector image number is different from from number of inputs!")
+        # require(depth_input.shape[2:] == (384, 384), "network has been configured for image of shape 384 x 384")
+
         # Resnet
-        layer_1 = self.backbone.layer1(x)
+        layer_1 = self.backbone.layer1(depth_input)
         layer_2 = self.backbone.layer2(layer_1)
         layer_3 = self.backbone.layer3(layer_2)
         layer_4 = self.backbone.layer4(layer_3)
@@ -90,27 +97,32 @@ class MidasHKRMNet(nn.Module):
         layer_3_rn = self.channel_reduc.layer3_rn(layer_3)
         layer_4_rn = self.channel_reduc.layer4_rn(layer_4)
 
-        features, num_objects, classes, masks = self.object_feature_extractor.get_object_features(
-            x, outputs=["features", "num_objects", "classes", "masks"])
+        layer_1_height, layer_1_width = layer_1_rn.shape[2:]
+        layer_2_height, layer_2_width = layer_2_rn.shape[2:]
+        layer_3_height, layer_3_width = layer_3_rn.shape[2:]
+        layer_4_height, layer_4_width = layer_4_rn.shape[2:]
 
-        obj_features = self._pad_image_features(features, num_objects)
-        masks = self._pad_masks(masks)
+        features, num_objects, classes, masks = self.object_feature_extractor.get_object_features(
+            object_detector_input, outputs=["features", "num_objects", "classes", "masks"])
+
+        obj_features = pad_features(features, num_objects, self.max_objects)
+        masks = pad_masks(masks, self.max_objects)
 
         # convert object features into feature maps
-        obj_features_1 = self.object_feature_mapper_1(
-            obj_features).reshape(-1, self.max_objects, ResNetSizes.LAYER_1.height, ResNetSizes.LAYER_1.width)
-        obj_features_2 = self.object_feature_mapper_2(
-            obj_features).reshape(-1, self.max_objects, ResNetSizes.LAYER_2.height, ResNetSizes.LAYER_2.width)
-        obj_features_3 = self.object_feature_mapper_3(
-            obj_features).reshape(-1, self.max_objects, ResNetSizes.LAYER_3.height, ResNetSizes.LAYER_3.width)
-        obj_features_4 = self.object_feature_mapper_4(
-            obj_features).reshape(-1, self.max_objects, ResNetSizes.LAYER_4.height, ResNetSizes.LAYER_4.width)
+        obj_features_1 = self._resize_features(obj_features, size=(layer_1_height, layer_1_width)).\
+            reshape(-1, self.max_objects, layer_1_height, layer_1_width)
+        obj_features_2 = self._resize_features(obj_features, size=(layer_2_height, layer_2_width)).\
+            reshape(-1, self.max_objects, layer_2_height, layer_2_width)
+        obj_features_3 = self._resize_features(obj_features, size=(layer_3_height, layer_3_width)).\
+            reshape(-1, self.max_objects, layer_3_height, layer_3_width)
+        obj_features_4 = self._resize_features(obj_features, size=(layer_4_height, layer_4_width)).\
+            reshape(-1, self.max_objects, layer_4_height, layer_4_width)
 
         # Resize masks
-        masks_4 = TF.resize(masks, size=[ResNetSizes.LAYER_4.height, ResNetSizes.LAYER_4.width])
-        masks_3 = TF.resize(masks, size=[ResNetSizes.LAYER_3.height, ResNetSizes.LAYER_3.width])
-        masks_2 = TF.resize(masks, size=[ResNetSizes.LAYER_2.height, ResNetSizes.LAYER_2.width])
-        masks_1 = TF.resize(masks, size=[ResNetSizes.LAYER_1.height, ResNetSizes.LAYER_1.width])
+        masks_4 = self._batch_resize_masks(masks, layer_4_rn.shape[2:])
+        masks_3 = self._batch_resize_masks(masks, layer_3_rn.shape[2:])
+        masks_2 = self._batch_resize_masks(masks, layer_2_rn.shape[2:])
+        masks_1 = self._batch_resize_masks(masks, layer_1_rn.shape[2:])
 
         decoder_input_4 = torch.cat((layer_4_rn, obj_features_4, masks_4), dim=1)
         decoder_input_3 = torch.cat((layer_3_rn, obj_features_3, masks_3), dim=1)
@@ -118,30 +130,19 @@ class MidasHKRMNet(nn.Module):
         decoder_input_1 = torch.cat((layer_1_rn, obj_features_1, masks_1), dim=1)
 
         # same as MiDaS, decoder
-        path_4 = self.scratch.refinenet4(decoder_input_4)
-        path_3 = self.scratch.refinenet3(path_4, decoder_input_3)
-        path_2 = self.scratch.refinenet2(path_3, decoder_input_2)
-        path_1 = self.scratch.refinenet1(path_2, decoder_input_1)
+        path_4 = self.refinenet_4(decoder_input_4)
+        path_3 = self.refinenet_3(path_4, decoder_input_3)
+        path_2 = self.refinenet_2(path_3, decoder_input_2)
+        path_1 = self.refinenet_1(path_2, decoder_input_1)
 
-        out = self.scratch.output_conv(path_1)
+        out = self.output_conv(path_1)
 
         return torch.squeeze(out, dim=1)
 
-    def _pad_masks(masks: List[torch.tensor]):
-        padded_masks = []
-        height, width = masks[0].shape[1:]  # assumes all masks have the same shape
-        for mask in masks:
-            padded_mask = torch.pad(mask[: self.max_objects], (0, 0, 0, 0, 0, max(0, self.max_objects - mask.shape[0])))
-            padded_masks.append(padded_mask)
-        return torch.cat(padded_masks).reshape(-1, self.max_objects, height, width)
+    def _batch_resize_masks(self, masks, size):
+        return torch.cat([TF.resize(m, size=size) for m in masks]).reshape(-1, self.max_objects, *size)
 
-    def _pad_image_features(obj_features: torch.tensor, num_objects: List[int]):
-        cur = 0
-        for num_obj in num_objects:
-            final_tensors.append(self._pad_zeros(obj_features[cur: cur + num_obj]))
-            cur += num_obj
-
-        return torch.cat(final_tensors)
-
-    def _pad_zeros(self, input_tensor):
-        return F.pad(input_tensor[:self.max_objects], (0, 0, 0, max(0, self.max_objects - input_tensor.shape[0]))
+    def _resize_features(self, features, size):
+        square_side = int(np.sqrt(self.object_feature_dimension))
+        feature_squares = features.reshape(features.shape[0], square_side, square_side)
+        return TF.resize(feature_squares, size)
