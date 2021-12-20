443bab1afe687099ceb7e8b59d1890c819f77c5d
Jellouli Ahmed <jellouli@iccluster138.iccluster.epfl.ch>
2021-12-20 20:38:26 +0100

    utils

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,4 +1,3 @@
-
 from midas.transforms import Resize, NormalizeImage, PrepareForNet
 import torch
 import torchvision.transforms as T
@@ -19,11 +18,15 @@ midas_train_transform = T.Compose(
         ),
         NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         PrepareForNet(),
-        lambda sample: torch.cat([torch.from_numpy(sample["image"]),
-                                 torch.from_numpy(sample["disparity"]).unsqueeze(0)]),
+        lambda sample: torch.cat(
+            [
+                torch.from_numpy(sample["image"]),
+                torch.from_numpy(sample["disparity"]).unsqueeze(0),
+            ]
+        ),
         T.RandomHorizontalFlip(p=0.5),
         T.RandomApply([T.RandomResizedCrop((384, 384))], p=0.3),
-        lambda tensor: (tensor[:3].unsqueeze(0), tensor[3].unsqueeze(0))
+        lambda tensor: (tensor[:3].unsqueeze(0), tensor[3].unsqueeze(0)),
     ]
 )
 
@@ -41,8 +44,10 @@ midas_eval_transform = T.Compose(
         ),
         NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         PrepareForNet(),
-        lambda sample: (torch.from_numpy(sample["image"]).unsqueeze(0),
-                        torch.from_numpy(sample["disparity"]).unsqueeze(0)),
+        lambda sample: (
+            torch.from_numpy(sample["image"]).unsqueeze(0),
+            torch.from_numpy(sample["disparity"]).unsqueeze(0),
+        ),
     ]
 )
 # Taken from midas code
@@ -62,4 +67,9 @@ midas_test_transform = T.Compose(
         PrepareForNet(),
         lambda sample: torch.from_numpy(sample["image"]).unsqueeze(0),
     ]
+)
+
+# Perform augmentation on a batch
+midas_batch_test_transform = lambda x: torch.cat(
+    [midas_test_transform(img) for img in x]
 )
--- src/utils/disp_utils.py
+++ src/utils/disp_utils.py
@@ -4,8 +4,12 @@ import numpy as np
 def map_disp_to_0_1(disp):
     res = disp.copy().astype(float)
     min_value, max_value = np.min(res), np.max(res)
-
-    return (res - min_value) / (max_value - min_value)
+    if min_value != max_value:
+        return (res - min_value) / (max_value - min_value)
+    elif max_value == 0:
+        return res
+    else:
+        return res / max_value
 
 
 def map_depth_to_disp(depth):
