57ceb264f873de1b7d2ce78c31130803ce400372
Jellouli Ahmed <jellouli@iccluster134.iccluster.epfl.ch>
2022-01-07 11:30:01 +0100

    docs midas_hkrm/zero_shot

--- midas_hkrm/zero_shot/criterion.py
+++ midas_hkrm/zero_shot/criterion.py
@@ -1,27 +1,54 @@
+import logging
+
 import torch
 import torch.nn as nn
 from midas_hkrm.utils import compute_scale_and_shift
-import logging
 
 logger = logging.getLogger()
 
+"""
+Much of this code was adapted from: https://gist.github.com/ranftlr/a1c7a24ebb24ce0e2f2ace5bce917022
+Note that this loss function are computed only on valid pixels
+"""
+
 
 class ComputeDepthThenCriterion(nn.Module):
+    """
+    Convenience criterion.
+    Given a disparity map, compute the corresponding depth map (with the least squares fitting method)
+    The depth is capped (anything more than the cap is set to the max), and then a loss function is computed
+    """
+
     def __init__(self, depth_cap: int, criterion: nn.Module):
+        """
+        Args:
+            depth_cap (int): maximum depth possible, anything more than this will be set to this maximum
+            criterion (nn.Module): Loss function in depth space
+        """
         super().__init__()
         self._depth_module = DispToDepth(depth_cap)
         self._criterion = criterion
 
-    def forward(self, prediction, target):
+    def forward(self, prediction: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         return self._criterion(self._depth_module(prediction, target), target)
 
 
 class DispToDepth(nn.Module):
-    def __init__(self, depth_cap):
+    """
+    Compute the depth map from a disparity map and a target depth map,
+    with the least squares criterion. Then cap the depth values at a maximum value.
+    """
+
+    def __init__(self, depth_cap: int):
+        """
+
+        Args:
+            depth_cap (int): maximum depth. Values will be capped at this maximum.
+        """
         super().__init__()
         self._depth_cap = depth_cap
 
-    def forward(self, prediction, target):
+    def forward(self, prediction: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         mask = target > 0
         logger.debug(f"Number of valid pixels {mask.sum()}")
 
@@ -41,14 +68,21 @@ class DispToDepth(nn.Module):
         return prediction_depth
 
 
-# Please see: https://gist.github.com/ranftlr/a1c7a24ebb24ce0e2f2ace5bce917022
-# code was adapted from this gist
 class BadPixelMetric(nn.Module):
-    def __init__(self, threshold):
+    """
+    Bad Pixel metric as computed for e.g, on NYUv2 (threshold is 1.25).
+    Please see the report for the mathematical details.
+    """
+
+    def __init__(self, threshold: float):
+        """
+        Args:
+            threshold (float): threshold after which a pixel is considered "wrong" (this is usually 1.25).
+        """
         super().__init__()
         self._threshold = threshold
 
-    def forward(self, prediction, target):
+    def forward(self, prediction: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         logger.debug(
             f"Computing BadPixelMetric from prediction of shape {prediction.shape} and target of shape {target.shape}"
         )
@@ -59,7 +93,7 @@ class BadPixelMetric(nn.Module):
         err[mask] = torch.max(
             prediction[mask] / target[mask],
             target[mask] / prediction[mask],
-        )
+        ).float()
 
         err[mask] = (err[mask] > self._threshold).float()
         logger.debug(f"Error sum {err.sum((1,2))}")
@@ -70,9 +104,15 @@ class BadPixelMetric(nn.Module):
 
 
 class AbsRel(nn.Module):
-    def forward(self, prediction, target):
+    """
+    Mean Absolute value of the relative error, as evaluated on ETH3D and TUM.
+    """
+
+    def forward(self, prediction: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         mask = target > 0
+
         err = torch.zeros_like(prediction, dtype=torch.float)
+
         err[mask] = ((prediction[mask] - target[mask]).abs() / target[mask]).float()
         p = torch.sum(err, (1, 2)) / torch.sum(mask, (1, 2))
 
@@ -80,13 +120,21 @@ class AbsRel(nn.Module):
 
 
 class MaskedRMSE(nn.Module):
-    def forward(self, prediction, target):
+    """
+    Root Mean Squared Error masked only on valid pixels.
+    """
+
+    def forward(self, prediction: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         mask = target > 0
         valid = mask.sum((1, 2)) > 0
-        if not torch.any(valid):
+
+        if not torch.any(valid):  # Some samples from BlendedMVS have all invalid pixels
             return 0
+
         prediction, target = prediction[valid], target[valid]
+
         err = torch.zeros_like(prediction, dtype=torch.float)
         err[mask] = ((prediction[mask] - target[mask]) ** 2).float()
+
         p = torch.sqrt(torch.sum(err, (1, 2)) / torch.sum(mask, (1, 2)))
         return torch.mean(p)
--- midas_hkrm/zero_shot/evaluator.py
+++ midas_hkrm/zero_shot/evaluator.py
@@ -6,12 +6,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms.functional as TF
 import tqdm
-from midas_hkrm.datasets import ETH3D, NYU, DIW, TUM
+from midas_hkrm.datasets import DIW, ETH3D, NYU, TUM
 from midas_hkrm.zero_shot import (
     AbsRel,
     BadPixelMetric,
     ComputeDepthThenCriterion,
-    DispToDepth,
 )
 from midas_hkrm.zero_shot.criterion import AbsRel
 from torch.utils.data import DataLoader, Dataset
@@ -21,12 +20,18 @@ logger = logging.getLogger(__name__)
 
 @dataclass(eq=False, frozen=True)
 class Evaluator:
-    model: nn.Module
-    criterion: nn.Module
-    transform: nn.Module
-    dataset: Dataset
-    device: str = "cuda"
-    pass_input: bool = False
+    """
+    Convenience class for running zero-shot evaluation on a MidasHKRM net.
+    This class always uses a batch size of 1 for simplicity, and computes the mean loss over all samples in the dataset.
+    Note that the prediction is bilinearly interpolated into the target size.
+    """
+
+    model: nn.Module  # model to test
+    criterion: nn.Module  # criterion to compute, usually from .criterion
+    transform: nn.Module  # input transform at test time
+    dataset: Dataset  # Dataset to test on
+    device: str = "cuda"  # device to run analysis on
+    pass_input: bool = False  # if True, the original image is passed to the model as well, as required in MidasHKRM
 
     def __post_init__(self):
         self.dataloader = DataLoader(
@@ -42,16 +47,19 @@ class Evaluator:
             loss = 0
             for index, batch in tqdm.tqdm(enumerate(self.dataloader)):
                 logger.debug(f"Processing input {index}")
-                input, target = batch[0]
-                input_t = self.transform(input).to(self.device)
 
+                input, target = batch[0]
+                # input transform
+                input_t = self.transform(input).to(self.device)
                 if self.pass_input:
                     prediction = self.model([input], input_t)
                 else:
                     prediction = self.model(input_t)
 
+                # Resize to desired shape
                 resized_prediction = TF.resize(prediction, size=input.shape[:2])
                 target = torch.tensor(target, dtype=float).to(self.device).unsqueeze(0)
+
                 step_loss = self.criterion(resized_prediction, target)
                 logger.debug(f"loss at step {index} is {step_loss}")
                 loss += step_loss
@@ -61,6 +69,11 @@ class Evaluator:
 
 
 class NyuV2Evaluator(Evaluator):
+    """
+    Compute the BadPixelMetric (with delta > 1.25) on the NYUv2 dataset.
+    The predicted depth is capped at 10, since this is the maximum depth in the dataset.
+    """
+
     def __init__(
         self,
         model: nn.Module,
@@ -88,6 +101,11 @@ class NyuV2Evaluator(Evaluator):
 
 
 class ETH3DEvaluator(Evaluator):
+    """
+    Compute the mean Absolute value of the relative error on ETH3D training set.
+    The predicted depth is capped at 72, since this is the maximum depth in the dataset.
+    """
+
     def __init__(
         self,
         model: nn.Module,
@@ -113,6 +131,11 @@ class ETH3DEvaluator(Evaluator):
 
 
 class TUMEvaluator(Evaluator):
+    """
+    Compute the mean Absolute value of the relative error on TUM training set.
+    The predicted depth is capped at 10, since this is the maximum depth in the dataset.
+    """
+
     def __init__(
         self,
         model: nn.Module,
@@ -138,6 +161,11 @@ class TUMEvaluator(Evaluator):
 
 
 class DIWEvaluator(Evaluator):
+    """
+    Compute the Ordinal Loss on the DIW test set.
+    This was not used in the final project.
+    """
+
     def __init__(
         self,
         model: nn.Module,
@@ -154,8 +182,7 @@ class DIWEvaluator(Evaluator):
             pass_input=pass_input,
         )
 
-    def loss(self, prediction, target):
+    def loss(self, prediction: torch.Tensor, target: torch.Tensor) -> int:
         sample = prediction[0]
         x1, y1, x2, y2 = target[0]
-
         return int(sample[int(x1), int(y1)] < sample[int(x2), int(y2)])
